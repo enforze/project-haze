2835f77edfe2057dfd8867b98dca304f26926636
DESKTOP-NLKUTHL\Connor <connorRmcrae@gmail.com>
2023-09-22 19:48:47 -0400

    typescript shenanigans good jumping off point please hire me dear god if you're looking at this

--- src/app/listItem.tsx
+++ src/app/listItem.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import Fab from '@mui/material/Button';
 import { TextField, Stack } from '@mui/material';
 import { AddCircleOutlineOutlined } from '@mui/icons-material';
+import {useForm, SubmitHandler} from 'react-hook-form';
+import handleSubmit from './listItemContainer';
 
 const TodoItem = {
     width: 'auto',
@@ -11,30 +13,33 @@ const TodoItem = {
     padding: '1em',
     height: '5em'
 }
-const SubmitButton = {
-    
+type FormValues = {
+    data : string
 }
-export default function listItem() {
-   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>)=> {
-        event.preventDefault();
-        const form = new FormData(event.currentTarget)
-        const formData = Object.fromEntries(form.entries());
-        console.log(formData);
+
+type listItemProps = {
+    mySubmit : (event :React.FormEvent<HTMLFormElement>)=>void
+}
+
+export default function ListItem({mySubmit}:listItemProps) {
+    const {list, handleSubmit} = useForm();
+    const onSubmit = async (event :React.FormEvent<HTMLFormElement>)=> {
+        mySubmit(event);
+        console.log("hello");
     }
     const handleChange = async (event: React.FormEvent<HTMLFormElement>)=> {
         event.preventDefault();
+
     }
     return (
         <Stack sx={{display: "flex", border: "none", width: "fit-content", marginTop: "1em", paddingLeft: "1em"}} direction='column' >
             <h1>I need to:</h1>
-       <form method="post" onSubmit={handleSubmit} onChange={handleChange}>
-        <TextField id="multiline"  multiline type="text" name="message" placeholder="input item" rows = {2}/> 
-        <Fab color="primary" aria-label="add" type="submit" style={{marginTop: "1.5em"}}>
-          <AddCircleOutlineOutlined />
-        </Fab>
-
- 
-        </form>
-        </Stack>
+            <form method="post" onSubmit={onSubmit} onChange={handleChange}>
+            <TextField id="multiline"  multiline type="text" name="message" placeholder="input item" rows = {2}/> 
+                <Fab color="primary" aria-label="add" type="submit" style={{marginTop: "1.5em"}}>
+                <AddCircleOutlineOutlined />
+                </Fab>
+            </form>
+            </Stack>
         );
   }
